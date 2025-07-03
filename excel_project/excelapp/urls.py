cc2a4ed4b574862fba654e5f345480f2a7cc4308
Jemin Bhatt <jeminbhatt5002@gmail.com>
2025-07-03 14:13:20 +0530

    Delete excel_project directory

--- excel_project/excelapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('upload/',views.upload_excel,name='upload_excel'),
-    path('process/',views.process_mapping,name='process_mapping')
-]
