2de07e2f8070caf84d14c2e6ec15470fb05a9cdf
Jemin Bhatt <jeminbhatt5002@gmail.com>
2025-07-04 13:38:56 +0530

    Update manage.py

--- excel_project/manage.py
+++ excel_project/manage.py
@@ -137,3 +137,13 @@ registration.html
     </div>
 </body>
 </html>
+
+app,urls
+
+from django.urls import path
+from .views import register, login_view
+
+urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', login_view, name='login'),
+]
